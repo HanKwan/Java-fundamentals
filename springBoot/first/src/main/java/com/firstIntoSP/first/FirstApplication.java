6bd2876bb8edee795f0ac98df9fec16ea9ae31be
Han Kwan Saing <saihankwansaing@gmail.com>
2026-05-20 19:49:10 +0900

    hello world

--- springBoot/first/src/main/java/com/firstIntoSP/first/FirstApplication.java
+++ springBoot/first/src/main/java/com/firstIntoSP/first/FirstApplication.java
@@ -3,18 +3,20 @@ package com.firstIntoSP.first;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-//@RestController
+@RestController
 public class FirstApplication {
 
 	public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(FirstApplication.class, args);
-
-        var myFirstService = context.getBean(MyFirstService.class);
-        System.out.println(myFirstService.useMethodFromMyFirstClass());
-        System.out.println(myFirstService.getCustomProperty());
-        System.out.println(myFirstService.getCustomInteger());
 	}
+
+    @GetMapping
+    public String hello() {
+        return "hello world";
+    }
 
 }
