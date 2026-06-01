30f917f4dba7313b7a15ae45190d287ba597cca5
Han Kwan Saing <saihankwansaing@gmail.com>
2026-06-02 00:12:58 +0900

    feat: created @PostMapping and tested in postman

--- springBoot/first/src/main/java/com/firstIntoSP/first/studentController.java
+++ springBoot/first/src/main/java/com/firstIntoSP/first/studentController.java
@@ -1,18 +1,29 @@
 package com.firstIntoSP.first;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/students")
 public class studentController {
 
-    @GetMapping("/students")
+    private List<Student> students = new ArrayList<>();
+
+    @PostMapping
+    public Student createStudent(@RequestBody Student student) {
+       students.add(student);
+       return student;
+    };
+
+    @GetMapping
     public List<Student> getStudetns() {
-        return List.of(
-                new Student(1L, "John", "Walton", "john@abcmail.com", 23)
-        );
+//        return List.of(
+//                new Student(1L, "John", "Walton", "john@abcmail.com", 23),
+//                new Student(2L, "Larry", "Lobster", "larry@abcmail.com", 25)
+//        );
+        return students;
     }
 
 }
