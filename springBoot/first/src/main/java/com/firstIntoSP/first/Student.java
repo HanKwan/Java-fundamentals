14137fa164b863c50d303b8eb0fc66736d1f2253
Han Kwan Saing <saihankwansaing@gmail.com>
2026-06-02 16:59:40 +0900

    refactor: config app.yml, setup jpa, postgres dependencies in pom.xml and added setter and empty constructor for student class

--- springBoot/first/src/main/java/com/firstIntoSP/first/Student.java
+++ springBoot/first/src/main/java/com/firstIntoSP/first/Student.java
@@ -1,14 +1,24 @@
 package com.firstIntoSP.first;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
 // @Component @Service, @Repository     // remove annotation when have Config file
+@Entity
 public class Student {
 
+    @Id
+    @GeneratedValue
     private Long id;
 
     private String firstName;
     private String lastName;
     private String email;
     private Integer age;
+
+    public Student() {
+    }
 
     public Student(Long id, String firstName, String lastName, String email, Integer age) {
         this.id = id;
@@ -36,5 +46,21 @@ public class Student {
 
     public Integer getAge() {
         return age;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
     }
 }
