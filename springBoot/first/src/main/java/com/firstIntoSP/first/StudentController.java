cc8f9da725f4c8ffe5ea67677e0c4530c7e6cbf8
Han Kwan Saing <saihankwansaing@gmail.com>
2026-06-02 19:03:14 +0900

    Use constructor injection for StudentRepository

--- springBoot/first/src/main/java/com/firstIntoSP/first/StudentController.java
+++ springBoot/first/src/main/java/com/firstIntoSP/first/StudentController.java
@@ -9,24 +9,29 @@ import java.util.List;
 @RequestMapping("/students")
 public class StudentController {
 
-    private List<Student> students = new ArrayList<>();
+    private final StudentRepository studentRepository;
+
+    public StudentController(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
 
     @PostMapping
     public Student createStudent(@RequestBody Student student) {
-       students.add(student);
-       return student;
+       return studentRepository.save(student);
     };
 
-    @GetMapping
-    public List<Student> getStudetns() {
-        return students;
+    
 
+//    @GetMapping
+//    public List<Student> getStudetns() {
+//        return students;
+//
 //        post students without db
 //        return List.of(
 //                new Student(1L, "John", "Walton", "john@abcmail.com", 23),
 //                new Student(2L, "Larry", "Lobster", "larry@abcmail.com", 25)
 //        );
-
-    }
+//
+//    }
 
 }
